00ee6bcaa7e64293b6114f735e947e1e3e27e653
Joshua Lynch <joshua.kevin.lynch@gmail.com>
2015-10-06 15:35:41 -0500

    reduced SNP count to 5 and process count to 2

--- lime/profile/mprof_lime_mpi_pbs.sh
+++ lime/profile/mprof_lime_mpi_pbs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#PBS -l walltime=06:00:00,pmem=2580mb,nodes=1:ppn=24
+#PBS -l walltime=00:10:00,pmem=2580mb,nodes=1:ppn=2
 #PBS -m abe
 #PBS -M lynch197@umn.edu
 #PBS -q mesabi
@@ -15,14 +15,14 @@ module list
 
 # change directory so mprof output will be written to the profile directory
 cd ~/lab/lime/lime/profile
-# -1 on the command line is written by mprof as -- -1 so instead use 20000 which guarantees all SNPs will be processed
+# -1 on the command line is written by mprof as -- -1 so instead specify a number larger than 0
 
-mpirun -n 24 \
+mpirun -n 2 \
     mprof run --interval=1.0 --nopython python ~/lab/lime/lime/lime.py \
         ~/lab/glowing-happiness/lime/project/hmp/16S_laf_sadj/data/taxa/finalotu.v35.anterior_nares.txt_laf_sadj_combined_collapsed.txt \
         ~/lab/glowing-happiness/lime/project/hmp/snp/exonic_snps_lasso_mpi.vcf \
         ~/lab/lime/lime/profile/mprof_lime_mpi_output.vcf \
         arcsinsqrt \
-        20000 \
+        5 \
         100 \
         no_permutation
