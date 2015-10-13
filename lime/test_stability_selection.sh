430f0ec25c9dcf9c60428b91e57a635dadb50793
Joshua Lynch <joshua.kevin.lynch@gmail.com>
2015-10-13 13:51:27 -0500

    changed output file extension

--- lime/test_stability_selection.sh
+++ lime/test_stability_selection.sh
@@ -3,7 +3,7 @@
 python stability_selection.py \
     ../example/lime_example_output.vcf \
     ../example/lime_example_taxon_table_input.txt \
-    ../example/stability_selection_example_output.txt \
+    ../example/stability_selection_example_output.vcf \
     0.3 \
     arcsinsqrt \
     -1
