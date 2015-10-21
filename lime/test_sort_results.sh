7d2eb1f2bd2641438d9e9a168aad2c740b4e2a38
jlynch <lynch197@umn.edu>
2015-10-21 11:49:14 +0100

    initial commit

---
+++ lime/test_sort_results.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+python sort_results.py \
+  ../example/stability_selection_example_output.vcf \
+  ../example/lime_example_taxon_table_input.txt \
+  arcsinsqrt \
+  0.05 \
+  0.5 \
+  10
