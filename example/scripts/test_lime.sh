fa5f657b937db82ae7019aaea2a2e64bff740db2
Joshua Lynch <joshua.kevin.lynch@gmail.com>
2016-08-05 17:53:47 +0100

    added --maf-lower-cutoff command line option

--- example/scripts/test_lime.sh
+++ example/scripts/test_lime.sh
@@ -5,9 +5,10 @@ export PYTHONPATH=../../lime:$PYTHONPATH
 mpirun -n 3 \
     python ../../lime/lime.py \
         ../data/lime_example_taxon_table_input.txt \
-        ../data/lime_example_snp_input.vcf \
-        ../lime_example_output.vcf \
+        ../data/lime_example_snp_input.rvcf \
+        ../lime_example_output.rvcf \
         arcsinsqrt \
         -1 \
         100 \
-        no_permutation
+        no_permutation \
+        --maf-lower-cutoff 0.2
