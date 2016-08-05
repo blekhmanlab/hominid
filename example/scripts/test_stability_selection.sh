175474095e40e76f02e36860ec546a65fd205d25
Joshua Lynch <joshua.kevin.lynch@gmail.com>
2016-08-05 17:55:54 +0100

    added --maf-lower-cutoff command line option

--- example/scripts/test_stability_selection.sh
+++ example/scripts/test_stability_selection.sh
@@ -3,9 +3,10 @@
 export PYTHONPATH=../../lime:$PYTHONPATH
 
 python ../../lime/stability_selection.py \
-    ../lime_example_output.vcf \
+    ../lime_example_output.rvcf \
     ../data/lime_example_taxon_table_input.txt \
-    ../stability_selection_example_output.vcf \
+    ../stability_selection_example_output.rvcf \
     0.3 \
     arcsinsqrt \
-    -1
+    -1 \
+    --maf-lower-cutoff 0.2
