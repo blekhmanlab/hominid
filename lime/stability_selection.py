edf313a0175ae15f1c738a641f92e8d30b587053
Joshua Lynch <joshua.kevin.lynch@gmail.com>
2016-08-05 17:49:54 +0100

    added command line option --maf-lower-cutoff

--- lime/stability_selection.py
+++ lime/stability_selection.py
@@ -33,6 +33,7 @@ def stability_selection_features_lasso_cv_C(
         output_file_path,
         lo_alpha_coef,
         transform,
+        maf_lower_cutoff,
         snp_limit
 ):
     # the input file path may be a glob
@@ -57,6 +58,7 @@ def stability_selection_features_lasso_cv_C(
             input_taxon_table_fp=taxon_table_input_file_path,
             output_vcf_fp='stability_selection_features_lasso_cv_C_dummy_output.txt',
             transform=transform,
+            maf_lower_cutoff=maf_lower_cutoff,
             snp_limit=-1,
             cv_count=100,
             permutation_method='none'
@@ -70,8 +72,6 @@ def stability_selection_features_lasso_cv_C(
                 if snp_lasso_task is None:
                     print('all done!')
                     break
-                # if we are working with synthetic data then do feature selection
-                # on all SNPs regardless of r squared ?
                 if snp_lasso_task.snp_with_rsq_df.rsq_pibsp_median_95ci_lo[0] > 0.0:
                     snp_count += 1
                     # found a SNP result for feature selection
@@ -85,14 +85,8 @@ def stability_selection_features_lasso_cv_C(
                     t1 = time.time()
                     print('time for feature selection: {:4.3f}s'.format(t1-t0))
 
-                    #print('before reindex: feature_scores_df.shape {}'.format(feature_scores_df.shape))
                     feature_scores_df = feature_scores_df.reindex(lasso_mpi.taxon_table_df.index)
-                    #print('after reindex: feature_scores_df.shape {}'.format(feature_scores_df.shape))
-                    # combine dataframes
-                    #print('snp_lasso_task.snp_with_rsq_df:\n{}'.format(snp_lasso_task.snp_with_rsq_df))
-                    #print('feature_scores_df:\n{}'.format(feature_scores_df))
                     snp_df = pd.concat([snp_lasso_task.snp_with_rsq_df, feature_scores_df.transpose()], axis=1)
-                    #print('combined data frame:\n{}'.format(df))
                     if snp_count == 1:
                         print('printing header')
                         snp_df.to_csv(
@@ -134,9 +128,8 @@ def select_features(aligned_snp_df, aligned_taxa_df, lo_alpha_coef):
         #random_state=13
     ).fit(X, y)
 
-    #print(clf.scores_)
     feature_scores_df = pd.DataFrame(clf.scores_, index=aligned_taxa_df.columns)
-    #print(feature_scores_df)
+
     return feature_scores_df, lars_cv.alphas_
 
 
@@ -150,6 +143,11 @@ if __name__ == '__main__':
         type=float
     )
     argparser.add_argument('transform')
+    argparser.add_argument(
+        '--maf-lower-cutoff',
+        type=float,
+        default=0.2
+    )
     argparser.add_argument(
         'snp_limit',
         type=int
