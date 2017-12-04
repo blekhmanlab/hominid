cb428aa41c867bb09c5e881065e02307a3226674
Joshua Lynch <joshua.kevin.lynch@gmail.com>
2017-12-03 21:23:03 -0500

    add pandas and statsmodels

--- setup.py
+++ setup.py
@@ -76,7 +76,15 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'scipy', 'scikit-learn', 'scikits.bootstrap', 'mpi4py'],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'scikit-learn',
+        'scikits.bootstrap',
+        'statsmodels',
+        'mpi4py'
+    ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
