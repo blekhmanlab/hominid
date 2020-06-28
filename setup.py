2826784ef20f354ee56388814da4e49b5cc95981
Joshua Lynch <joshua.kevin.lynch@gmail.com>
2020-06-27 20:06:22 -0400

    update version to 1.1.0

--- setup.py
+++ setup.py
@@ -17,13 +17,16 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+with open('requirements.txt') as f:
+    requirements = f.read().split()
+
 setup(
     name='hominid',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='1.1.0',
 
     description='Python MPI program using LASSO regression to find associations between host genetics and microbiome.',
     long_description=long_description,
@@ -33,7 +36,7 @@ setup(
 
     # Author details
     author='Joshua Lynch',
-    author_email='lynch197@umn.edu',
+    author_email=None,
 
     # Choose your license
     license='MIT',
@@ -55,8 +58,6 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
     ],
@@ -76,15 +77,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
-        'numpy',
-        'pandas',
-        'scipy',
-        'scikit-learn',
-        'scikits.bootstrap',
-        'statsmodels',
-        'mpi4py'
-    ],
+    install_requires=requirements,
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
